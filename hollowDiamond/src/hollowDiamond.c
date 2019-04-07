65732b712e3a90de3a5550552c0ed48b6ba35a91
jamesmac <jamesmac@192.168.2.20>
2019-04-07 14:23:51 -0400

    Fixed bug, should have error proof input

--- hollowDiamond/src/hollowDiamond.c
+++ hollowDiamond/src/hollowDiamond.c
@@ -21,6 +21,9 @@ void printTopPyramid(int row, int top, int col);
 void printBottomPyramid(int row, int bottom, int col);
 int yes(void);
 void clearBuffer(void);
+void valPos(int *num, const char text[]);
+int getInt(void);
+int getIntInRange(int min, int max);
 
 int outline = 0;
 
@@ -29,12 +32,13 @@ int main(void) {
 	int num = 0, again;
 	do{
 		printf("Enter the number of lines: ");
-		scanf(" %d", &num);
+		num = getInt();
+		valPos(&num, "");
 
 		printf("Do you want a hollow pyramid? (y or n) ");
 		if(yes()){
 			printf("Enter the thickness of border: (%d - %d): ", 1, (num / 2 -1));
-			scanf(" %d", &outline);
+			outline = getIntInRange(1, (num / 2 - 1));
 		}
 
 		printDiamond(num);
@@ -125,7 +129,7 @@ int yes(void){
 	char nl;
 
 	do {// validate that user enters (y or n) or (Y or N)
-		clearBuffer();
+
 		scanf(" %c%c", &choice, &nl);
 		if (nl == '\n') {
 			if (choice == 'n' || choice == 'N'){
@@ -143,3 +147,52 @@ int yes(void){
 
 	return res; // 1 true for y and Y, 0 false for n or N
 }
+void valPos(int *num, const char text[]){
+
+	while(*num < 1){	// ensure that user has entered a positive number
+		printf("*** INVALID %s NUMBER *** <must be a positive number>: ", text);
+		*num = getInt();
+	}
+	return;
+}
+
+// get a valid integer without characters or any other garbage input
+int getInt(void){
+	int res;
+	int valid = -1;
+	int num;
+	char nl;
+
+	do{
+		scanf(" %d%c", &num, &nl);
+		if (nl == '\n'){
+			res = num;
+			valid = 1;
+		} else {
+			clearBuffer();
+			printf("*** INVALID INTEGER *** <Please enter an integer>: ");
+		}
+	} while (valid < 0);
+
+	return res;
+}
+int getIntInRange(int min, int max){
+	int res;
+	int num;
+	int valid = -1;
+
+	do {
+		num = getInt();
+
+		if (num >= min && num <= max){
+			res = num;
+			valid = 1;
+		} else {
+			printf("*** OUT OF RANGE *** <Enter a number between %d and %d>: ", min, max);
+		}
+
+	} while (valid < 0);
+
+	return res;  // return integer within range min - max
+}
+
